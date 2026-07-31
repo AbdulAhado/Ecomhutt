2b2704bdd701238fd3918c92f7a2e9ab391d1ced
AbdulAhado <devilrao125@gmail.com>
2026-07-31 17:06:11 +0500

    Fix layout import

--- src/app/(store)/layout.js
+++ src/app/(store)/layout.js
@@ -1,5 +1,5 @@
-import Navbar from '@/components/layout/Navbar';
-import Footer from '@/components/layout/Footer';
+import Navbar from '@/components/Layout/Navbar';
+import Footer from '@/components/Layout/Footer';
 import ScrollToTop from '@/components/ScrollToTop';
 
 export default function StoreLayout({ children }) {
